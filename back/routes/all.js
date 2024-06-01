dd4a49cd94e976d9dd01bdb166c5c1c5298864ae
jjmchew <jjmchew@gmail.com>
2024-06-01 12:55:21 -0600

    re-factored allRouter routes

--- back/routes/all.js
+++ back/routes/all.js
@@ -5,59 +5,49 @@ const { useMongo } = require('../db/mongo.js');
 const { usePg } = require('../db/postgres.js');
 const util = require('../lib/utils.js');
 
-const routerFct = (req, res, next) => {
-  console.log('routerFct ', req.subdomains, req.method, req.path);
-  next();
+const routeToReact = (res) => {
+  const newPath = path.join('/root/miniProj/front/dist/index.html');
+  console.log('routeToReact: ', newPath);
+  res.sendFile(newPath, err => {
+      if (err) res.status(500).send(err)
+  });
 };
 
+const processBin = async (req) => {
+  const binHash = req.subdomains[0];
+  console.log('processBin: ', binHash, req.method);
+
+  if (!util.isBinHash(binHash)) throw new Error('Invalid bin hash');
+
+  const requestHash = util.makeHash(false);
+  console.log('allRouter: ', req.method, 'bin: ', binHash, 'request', requestHash);
+
+  await Promise.all([
+    usePg.putRequest(binHash, requestHash, req),
+    useMongo.putRequest(requestHash, req),
+  ])
+  .catch( _err => { throw new Error('Request could not be saved') });
+
+  return { binHash: binHash, requestHash: requestHash };
+};
+
+
+
 // test
-allRouter.all('*', (req, res, next) => {
-  console.log('== * :', req.subdomains, req.method, req.path);
-});
-
-allRouter.put('/*', (req, res, next) => {
-  console.log('== allRouter put /* :', req.subdomains, req.method, req.path);
-  next();
-});
-
-allRouter.all('/*', (req, res, next) => {
-  console.log('== allRouter all /* :', req.subdomains, req.method, req.path);
-  if (req.subdomains.length === 0) {
-    const newPath = path.join('/root/miniProj/front/dist/index.html');
-    console.log('allRouter - no subdomains, passing to index.html', newPath);
-    res.sendFile(newPath, (err) => {
-      if (err) res.status(500).send(err)
-    });
-  } else {
-    console.log('allRouter - has subdomains should pass to next route with bin');
-    next();
-  }
-});
-
-
 
 // receives webhooks and stores them with appropriate binHash
+allRouter.all('/*', async (req, res, next) => {
+  console.log('== allRouter all /* :', req.subdomains, req.method, req.path);
+  if (req.subdomains.length === 0) routeToReact(res);
 
-allRouter.all('/', async (req, res, next) => {
-  const binHash = req.subdomains[0];
-  console.log('== allRouter / (process webhooks route) : ', binHash, req.method);
   try {
-    if (!util.isBinHash(binHash)) throw new Error('Invalid bin hash');
-  
-    const requestHash = util.makeHash(false);
-    console.log('allRouter: ', req.method, 'bin: ', binHash, 'request', requestHash);
-  
-    await Promise.all([
-      usePg.putRequest(binHash, requestHash, req),
-      useMongo.putRequest(requestHash, req),
-    ])
-    .catch( _err => { throw new Error('Request could not be saved') });
-  
-    res.status(200).send({ binHash: binHash, requestHash: requestHash });
+    const result = await processBin(req);
+    res.status(200).send(result);
   } catch (e) {
-    console.error('>>>>> allRouter error', e.message);
+    console.error('>>>>> allRouter error ', e.message);
     next(e);
   }
 });
+
 
 module.exports = allRouter;
